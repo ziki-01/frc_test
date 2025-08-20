557fa969cd3086a2edc8bb081b965eab30698703
ziki-01 <ziki_01@126.com>
2025-08-20 19:02:29 +0800

    position_velocity_test

--- src/main/java/frc/robot/subsystems/drive.java
+++ src/main/java/frc/robot/subsystems/drive.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import java.security.PublicKey;
+
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
@@ -15,6 +17,7 @@ import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.Led1OffColorValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
 
+import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -23,17 +26,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class drive extends SubsystemBase {
 
   //声明电机
-  private final TalonFX m_test_motor = new TalonFX(1, "rio");
-  // private final TalonFX m_test_motor2 = new TalonFX(2, "rio");
+  private final TalonFX m_test_motor = new TalonFX(5, "rio");
+  private final TalonFX m_test_motor2 = new TalonFX(6, "rio");
   // private final TalonFX m_test_motor3 = new TalonFX(3, "rio");
   // private final TalonFX m_test_motor4 = new TalonFX(4, "rio");
   //特性：请求制，需要一个request
   private final MotionMagicVoltage m_test_motor_request = new MotionMagicVoltage(0.0);
-  // private final MotionMagicVoltage m_test_motor2_request = new MotionMagicVoltage(0.0);
+  private final VelocityTorqueCurrentFOC m_test_motor2_request = new VelocityTorqueCurrentFOC(0.0);
   // private final MotionMagicVoltage m_test_motor3_request = new MotionMagicVoltage(0.0);
   // private final MotionMagicVoltage m_test_motor4_request = new MotionMagicVoltage(0.0);
 
-  private final CANcoder cancoder_fl = new CANcoder(1, "rio");
+  private final CANcoder cancoder_fl = new CANcoder(3, "rio");
   
   //实际控制
   //封装出来的方法
@@ -46,6 +49,11 @@ public class drive extends SubsystemBase {
 
   public void setmotorPosition(double pos) {
     m_test_motor.setControl(m_test_motor_request.withPosition(pos));
+    // m_test_motor2.setControl(m_test_motor2_request.withPosition(vol));
+  }
+
+  public void setmotorVelocity(double vol) {
+    m_test_motor2.setControl(m_test_motor2_request.withVelocity(vol));
     // m_test_motor2.setControl(m_test_motor2_request.withPosition(vol));
   }
 
@@ -66,10 +74,33 @@ public class drive extends SubsystemBase {
   //   reture (Math.abs(expected_position-current_position))
   // }
 
-  public Command Motor_Position_command(double  Position){
-    return runOnce(()->{
+  double motorPosition = 0.0;
+  double targetPosition = 0.0;
+  double acceptableError = 0.2;
+
+  public boolean IsAtPosition(double Position){
+    motorPosition = m_test_motor.getPosition().getValueAsDouble(); // Get the current position of the motor
+
+    if(Math.abs(motorPosition - Position) <= acceptableError) {
+      return true; // The motor is within the acceptable error range of the target position
+    } else {
+      return false; // The motor is not at the target position
+    }
+  }
+
+  public Command Motor_Position_Velocity_command(double  Velocity,double  Position){
+    return run(()->{
       setmotorPosition(Position); // Set the motor to move at 1000 units per second
+      setmotorVelocity(Velocity); 
+    }).until(() -> IsAtPosition(Position));
+  }
+
+  public Command Motor_Velocity_command(double  Velocity){
+    return runOnce(()->{
+      setmotorVelocity(Velocity);// Set the motor to move at 1000 units per second
     });
+      
+    
   }
 
   public drive() {
@@ -82,13 +113,6 @@ public class drive extends SubsystemBase {
 
     var motorConfigs = new TalonFXConfiguration();
     motorConfigs.Feedback.RotorToSensorRatio = 13;
-
-    motorConfigs.Slot0.kS = 0.2;
-    motorConfigs.Slot0.kV = 0.0;
-    motorConfigs.Slot0.kA = 0;
-    motorConfigs.Slot0.kP = 3;
-    motorConfigs.Slot0.kI = 0;
-    motorConfigs.Slot0.kD = 0;
 
     // 手动调整通常遵循以下过程：
 
@@ -113,6 +137,12 @@ public class drive extends SubsystemBase {
     // 尽可能增加，不要给响应带来抖动。
     //逐步增加kd直到引入了新的震动
 
+    motorConfigs.Slot0.kS = 0.2;
+    motorConfigs.Slot0.kV = 0.0;
+    motorConfigs.Slot0.kA = 0;
+    motorConfigs.Slot0.kP = 3;
+    motorConfigs.Slot0.kI = 0;
+    motorConfigs.Slot0.kD = 0;
 
     motorConfigs.MotionMagic.MotionMagicAcceleration = 100; // Acceleration is around 40 rps/s
     motorConfigs.MotionMagic.MotionMagicCruiseVelocity = 200; // Unlimited cruise velocity
@@ -120,10 +150,33 @@ public class drive extends SubsystemBase {
     motorConfigs.MotionMagic.MotionMagicExpo_kA = 0.1; // Use a slower kA of 0.1 V/(rps/s)
     motorConfigs.MotionMagic.MotionMagicJerk = 0; // Jerk is around 0
 
+
+    var motorEncoderConfigs2 = new CANcoderConfiguration();
+    motorEncoderConfigs2.MagnetSensor.MagnetOffset=0.0;//offset
+    motorEncoderConfigs2.MagnetSensor.AbsoluteSensorDiscontinuityPoint=0.5;
+    motorEncoderConfigs2.MagnetSensor.SensorDirection=SensorDirectionValue.Clockwise_Positive;
+
+    var motorConfigs2 = new TalonFXConfiguration();
+
+    motorConfigs2.Slot0.kS = 1.3;
+    motorConfigs2.Slot0.kV = 0.0;
+    motorConfigs2.Slot0.kA = 0;
+    motorConfigs2.Slot0.kP = 7;
+    motorConfigs2.Slot0.kI = 0;
+    motorConfigs2.Slot0.kD = 0.1;
+
+    motorConfigs2.MotionMagic.MotionMagicAcceleration = 100; // Acceleration is around 40 rps/s
+    motorConfigs2.MotionMagic.MotionMagicCruiseVelocity = 200; // Unlimited cruise velocity
+    motorConfigs2.MotionMagic.MotionMagicExpo_kV = 0.12; // kV is around 0.12 V/rps
+    motorConfigs2.MotionMagic.MotionMagicExpo_kA = 0.1; // Use a slower kA of 0.1 V/(rps/s)
+    motorConfigs2.MotionMagic.MotionMagicJerk = 0; // Jerk is around 0
+
     motorConfigs.Feedback.FeedbackRemoteSensorID = cancoder_fl.getDeviceID();
     motorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
 
     m_test_motor.getConfigurator().apply(motorConfigs);
+    m_test_motor2.getConfigurator().apply(motorConfigs2);
+
     // m_test_motor2.getConfigurator().apply(motorConfigs);
     // m_test_motor3.getConfigurator().apply(motorConfigs);
     // m_test_motor4.getConfigurator().apply(motorConfigs);
